f5718c8c80e613cbeb68159c29510b55328ab9ad
cliffamadeus <dev.clfm5t12@gmail.com>
2026-08-27 08:08:31 +0800

    Added queries for borrow and return

--- lab3.sql
+++ lab3.sql
@@ -11,7 +11,7 @@ CREATE TABLE borrow (
     student_id INT NOT NULL,
     book_id INT NOT NULL,
     borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    borrow_return_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    borrow_return_date TIMESTAMP NULL DEFAULT NULL,
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES students(student_id),
     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
@@ -19,9 +19,19 @@ CREATE TABLE borrow (
 SELECT br.borrow_id, s.student_id,
     CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name, s.student_course,
     b.book_title, b.book_author, b.book_category,
-    br.borrow_date 
+    br.borrow_date,br.borrow_return_date 
 FROM borrow br
     JOIN students s ON br.student_id = s.student_id
     JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NULL
 ORDER BY br.borrow_date DESC;
 
+-- Alter the borrow_return_date column to allow NULL values and set the default to NULL
+ALTER TABLE borrow
+MODIFY borrow_return_date TIMESTAMP NULL DEFAULT NULL;
+
+UPDATE borrow 
+SET borrow_return_date = NULL
+WHERE borrow_return_date = '2026-08-25 08:15:50';
+
+-- Return a book by updating the borrow_return_date to the current timestamp
